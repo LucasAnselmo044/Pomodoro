44f02888b6cf50fe5e567277babc02b4e011705d
Lucas <xablau2007pvp@gmail.com>
2025-06-14 02:28:49 -0300

    minor changes

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var botaoStart = document.getElementById("start");
 
+let tempo = 25;
+let cronometro = cronometro * 60;
+
 function comecarCronometro() {}
 
 botaoStart.addEventListener("click", comecarCronometro);
